9a9825dbd1dc24a49497d46766eb2ed5c1720a7c
juicermv <34755463+juicermv@users.noreply.github.com>
2025-04-26 15:54:23 +0300

    Packaging stuff

--- crates/lemm_app/src/main.rs
+++ crates/lemm_app/src/main.rs
@@ -1,3 +1,5 @@
+#![windows_subsystem = "windows"]
+
 mod app;
 mod components;
 mod data;
@@ -6,7 +8,16 @@ mod route;
 mod server;
 
 use app::App;
+use dioxus::{
+    desktop::{Config, WindowBuilder},
+    LaunchBuilder,
+};
 
 fn main() {
-    dioxus::launch(App);
+    let window = WindowBuilder::new()
+        .with_title("LE Mod Manager")
+        .with_resizable(true);
+    let cfg = Config::new().with_menu(None).with_window(window);
+
+    LaunchBuilder::desktop().with_cfg(cfg).launch(App);
 }
